70450aa07c893c2b73fdb8c5ca1059ebb7003921
KhaldoonKH <khaldoon9kh9@gmail.com>
2024-08-08 17:16:43 +0300

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './containers/NavBar/index.jsx'; 
+import LandingPage from './containers/LandingPage/index.jsx';  
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <LandingPage />
     </div>
   );
 }
---
+++ src/containers/LandingPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import LandingImg from '../../imgs/landingImg.webp'
+
+const LandingPage = () => {
+    return (
+        <div className='landingImgCont'>
+            <img src={LandingImg} alt="Landing Image" />
+        </div>
+    );
+};
+
+export default LandingPage;
