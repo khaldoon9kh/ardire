1b1c60fdd605b6cdda2f49764c76d010be41fce4
KhaldoonKH <khaldoon9kh9@gmail.com>
2024-08-15 13:37:29 +0300

    eader complete

--- src/components/CategoryHeader/index.jsx
+++ src/components/CategoryHeader/index.jsx
@@ -4,8 +4,13 @@ import LandingImg from "../../imgs/landingImg.webp"
 
 const CategoryHeader = () => {
     return (
-        <div className='landingImgCont'>
+        <div className='categoryHeaderCont'>
             <img src={LandingImg} alt="Landing Image" />
+            <div className="category-header-text">
+                <h1>Rings</h1>
+                <p>Shop spectacular diamond rings at Graff. Each set with a unique design incorporating exceptionally rare diamonds & gemstones in unique settings.
+                </p>
+            </div>
         </div>
     );
 };
